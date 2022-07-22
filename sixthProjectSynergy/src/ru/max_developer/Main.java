913b0a4ccad24fcf4df38a081d0e4f130d3bbd14
Maxim Novikov <60720903+maxxx1502@users.noreply.github.com>
2022-07-22 22:40:06 +1000

    Add files via upload

---
+++ sixthProjectSynergy/src/ru/max_developer/Main.java
@@ -0,0 +1,35 @@
+package ru.max_developer;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.LinkedList;
+// Напишите метод, который добавляет 1000000 элементов
+// в ArrayList и LinkedList. Напишите еще один метод,
+// который выбирает из заполненного списка элемент наугад 100000 раз.
+// Замерьте время, которое потрачено на это.
+// Сравните результаты и предположите, почему они именно такие.
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static void compare2Lists(){
+        ArrayList<Double> arrayList = new ArrayList <> ();
+        LinkedList<Double> linkedList = new LinkedList <> ();
+        final int N = 1000000;
+        final int M = 1000;
+        for(int i = 0; i < N; i++) {
+            arrayList.add(Math.random());
+            linkedList.add(Math.random());
+        }
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i < M; i++) {
+            arrayList.get((int) (Math.random() * (N -1)));
+        }
+        System.out.println(System.currentTimeMillis() -startTime);
+        startTime = System.currentTimeMillis();
+        for(int i = 0; i < M; i++) {linkedList.get((int) (Math.random() * (N -1)));
+        }
+        System.out.println(System.currentTimeMillis() -startTime);
+    }
+}
